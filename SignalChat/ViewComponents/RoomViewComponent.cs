ed5aefab8af67f7cec8b9280688b584ffb62c471
SuchABadCoder <gayday57@gmail.com>
2021-08-18 12:37:19 +0300

    Room partial view

---
+++ SignalChat/ViewComponents/RoomViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalChat.Data;
+using System.Linq;
+
+namespace SignalChat.ViewComponents
+{
+    public class RoomViewComponent : ViewComponent
+    {
+        private ApplicationDbContext _context;
+
+        public RoomViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var chats = _context.Chats.ToList();
+            return View(chats);
+        }
+    }
+}
